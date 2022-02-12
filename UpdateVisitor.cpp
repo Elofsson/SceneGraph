fa890f4102722e4b90e60259a19410e3605e4418
Elof Fahlstedt <dv19eft@cs.umu.se>
2022-02-12 14:41:37 +0100

    Fixed UpdateVisitor

--- UpdateVisitor.cpp
+++ UpdateVisitor.cpp
@@ -1,19 +1,18 @@
 #include "UpdateVisitor.h"
 
-void UpdateVisitor::visit(Transform &t)
-{
-  //std::cout << "UpdateVisitor::Visit Transform" << std::endl;
-  t.executeCallbacks();
-}
-void UpdateVisitor::visit(Geometry &g)
-{
-  //std::cout << "UpdateVisitor::Visit Geometry" << std::endl;
-  g.executeCallbacks();
-}
-
 void UpdateVisitor::visit(Group &g)
 {
-  //std::cout << "UpdateVisitor::VisitGroup" << std::endl;
   g.executeCallbacks();
   NodeVisitor::visit(g);
+}
+
+void UpdateVisitor::visit(Transform &t)
+{
+  t.executeCallbacks();
+  NodeVisitor::visit(t);
+}
+
+void UpdateVisitor::visit(Geometry &g)
+{
+  g.executeCallbacks();
 }
